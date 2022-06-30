412a86aefbdcde4c6fbf1d60704d31ef0564887f
ritu-frontdev <108183379+ritu-frontdev@users.noreply.github.com>
2022-06-30 18:50:16 +0530

    Update infinite_image.js

--- infinite_image.js
+++ infinite_image.js
@@ -1,5 +1,5 @@
 var container = document.getElementById("container")
-const url = "https://api.unsplash.com/photos/random/?client_id=8jP3xQitHC3uClSdvWSwyrZVguDmUf_Q1L1pVTz1b7U&count=10&query=cartoons"
+const url = "https://api.unsplash.com/photos/random/?client_id={secrets.API_KEY1}&count=10&query=cartoons"
 function getdata(){
     fetch(url).then(response=>response.json()).then(data=>{
         //console.log(data);
